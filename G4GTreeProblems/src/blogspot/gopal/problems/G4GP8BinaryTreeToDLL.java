56c74ef1d62d1f51cf9db4d5b60d13884063052a
dsalgorithmist <dsalgorithmist@gmail.com>
2016-07-01 07:13:47 +0530

    BTreeToDLL Solved

--- G4GTreeProblems/src/blogspot/gopal/problems/G4GP8BinaryTreeToDLL.java
+++ G4GTreeProblems/src/blogspot/gopal/problems/G4GP8BinaryTreeToDLL.java
@@ -13,51 +13,53 @@ import blogspot.gopal.util.visual.BetterBTreePrinter;
  *
  */
 public class G4GP8BinaryTreeToDLL {
+	
+	// 'head' - reference to head node of created
+	// double linked list
+	private static BTNode<Integer> head;
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		BTNode<Integer> root = BinaryTreeCreator.createBinaryTree(
-				new Integer[] { 8, 3, 10, 1, 6, 100, 14, -2, -3, 4, 7, -4, -5, 13 }, new Integer[] { -2, -3, -4, -5 });
+				new Integer[] { 8, 3, 10, 1, 6, 100, 14, -2, -3, 4, 7, -4, -5,
+						13 }, new Integer[] { -2, -3, -4, -5 });
 		BetterBTreePrinter.printNode(root);
 		BTreePrinter.printNode(root);
-		convertToDLL(root);
+		
+		BToDLL(root);
+		printList(head);
 	}
 
-	private static void convertToDLL(BTNode<Integer> root) {
-		convertToDLLInternal(root, null);
-	}
 
-	private static void convertToDLLInternal(BTNode<Integer> node, BTNode<Integer> parentNode) {
-		if (node == null) {
+	static void BToDLL(BTNode<Integer> root) {
+		// Base cases
+		if (root == null)
 			return;
-		}
-		if (node.getLeft() != null) {
-			convertToDLLInternal(node.getLeft(), node);
-		}
-		if (parentNode != null) {
-			System.out.println("Visiting : " + node.getData());
-			boolean isLeftNode = parentNode.getLeft() == node;
-			if (isLeftNode) {
-				node.setLeft(parentNode);
-				parentNode.setRight(node);
-			} else {
-				node.setRight(parentNode);
-			}
-		} else {
-			BTNode<Integer> tempLeft = node.getLeft();
-			BTNode<Integer> tempRight = node.getRight();
-			if (tempLeft != null) {
-				tempLeft.setLeft(node);
-				node.setRight(tempLeft);
-			}
-			if (tempRight != null) {
-				tempRight.setRight(node);
-			}
-		}
-		if (node.getRight() != null) {
-			convertToDLLInternal(node.getRight(), node);
+
+		// Recursively convert right subtree
+		BToDLL(root.getRight());
+
+		// insert root into DLL
+		root.setRight(head);
+
+		// Change left pointer of previous head
+		if (head != null)
+			head.setLeft(root);
+
+		// Change head of Doubly linked list
+		head = root;
+
+		// Recursively convert left subtree
+		BToDLL(root.getLeft());
+	}
+
+	static void printList(BTNode<Integer> head) {
+		System.out.println("Extracted Double Linked List is : ");
+		while (head != null) {
+			System.out.print(head.getData() + " ");
+			head = head.getRight();
 		}
 	}
 
